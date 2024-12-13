75612ff115dc0525d700e4b2ff146b565a1b5319
Bryne Carruthers <63172116+DrCBeatz@users.noreply.github.com>
2024-12-13 13:30:40 -0500

    Specify form labels in assistant/forms.py

--- assistant/forms.py
+++ assistant/forms.py
@@ -3,17 +3,21 @@ from django import forms
 
 class QuestionForm(forms.Form):
     question = forms.CharField(
+        label="",
         widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Ask a question..."})
     )
     file = forms.FileField(
         required=False,
+        label="",
         widget=forms.ClearableFileInput(attrs={"class": "form-control-file"})
     )
     apply_time = forms.DateTimeField(
         required=False,
+        label="Schedule Start",
         widget=forms.DateTimeInput(attrs={"type": "datetime-local", "class": "form-control"})
     )
     revert_time = forms.DateTimeField(
         required=False,
+        label="Schedule End",
         widget=forms.DateTimeInput(attrs={"type": "datetime-local", "class": "form-control"})
     )
