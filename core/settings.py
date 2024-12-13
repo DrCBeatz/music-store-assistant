2c8d94322fe758d730ec5a4124b8ea85640defcd
Bryne Carruthers <63172116+DrCBeatz@users.noreply.github.com>
2024-12-13 14:08:47 -0500

    Add LOGIN_REDIRECT_URL to settings.py

--- core/settings.py
+++ core/settings.py
@@ -108,6 +108,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 # Internationalization
