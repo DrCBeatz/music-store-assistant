f9bcf4a5c86e042b0b89d61f9d56c25b1bdbf08e
Bryne Carruthers <63172116+DrCBeatz@users.noreply.github.com>
2025-03-08 16:55:52 -0500

    Update celery.py

--- core/celery.py
+++ core/celery.py
@@ -9,6 +9,9 @@ app = Celery("core")
 # Load config from Django settings
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
+# limit concurrency to 1 worker
+app.conf.update(worker_concurrency=1)
+
 # Set Celery configuration option
 app.conf.broker_connection_retry_on_startup = True
 
